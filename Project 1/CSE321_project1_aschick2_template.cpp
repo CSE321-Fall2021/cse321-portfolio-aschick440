25a1666178ae42e87bc2cd5779aac33176099426
Andrew Schick <59938136+aschick440@users.noreply.github.com>
2021-10-02 16:27:44 -0400

    Add files via upload

---
+++ Project 1/CSE321_project1_aschick2_template.cpp
@@ -0,0 +1,30 @@
+/**
+    File:   CSE321_project1_aschick2_template.c
+
+    Author1:        Andrew Schick (aschick2@buffalo.edu)
+    Date:           Fall 2021
+    Partner:        Solo submission
+    Course:         CSE 321 - Embedded Systems
+    Project:        project 1 Getting Started
+    Modules:        main
+    Inputs:         None
+    Outputs:        None
+    Constraints:    None 
+
+    File Summary:
+        This is used as a header file template for future Mbed projects.
+
+    References:     
+        - UB CSE 321 Lecture Slide 3
+**/
+
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+        
+    }
+}
+
